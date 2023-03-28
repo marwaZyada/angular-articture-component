f7965cbbc46890c19098b2cc00a9294183bcbf4a
marwaZyada <zyadamarwa6@gmail.com>
2023-03-29 01:56:39 +0200

    update modal

--- src/app/Component/portfolio/portfolio.component.ts
+++ src/app/Component/portfolio/portfolio.component.ts
@@ -7,11 +7,11 @@ import { Component } from '@angular/core';
 })
 export class PortfolioComponent {
 images:object[]=
-[{src:"assets/images/cabin.png", label:"exampleModal1"},
-{src:"assets/images/cake.png",label:"exampleModal2"},
-{src:"assets/images/circus.png",label:"exampleModal3"},
-{src:"assets/images/game.png",label:"exampleModal4"},
-{src:"assets/images/safe.png",label:"exampleModal5"},
-{src:"assets/images/submarine.png",label:"exampleModal6"}]
+[{src:"assets/images/cabin.png", label:"exampleModal1",name:"log cabin"},
+{src:"assets/images/cake.png",label:"exampleModal2",name:"tasty cake"},
+{src:"assets/images/circus.png",label:"exampleModal3",name:"circus tent"},
+{src:"assets/images/game.png",label:"exampleModal4",name:"controller"},
+{src:"assets/images/safe.png",label:"exampleModal5",name:"locked safe"},
+{src:"assets/images/submarine.png",label:"exampleModal6",name:"submarine"}]
 
 }
